bcfe4080955fdad87ff8a3275d1a265aa512f10a
tomasci <zivgta@gmail.com>
2021-08-27 23:40:13 +0300

    GoogleAnalytics deep integration

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,26 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang={"en"}>
-                <Head/>
+                <Head>
+                    <title/>
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                                  page_path: window.location.pathname,
+                                });
+                              `,
+                        }}
+                    />
+                </Head>
                 <body>
                     <noscript>You need to enable JavaScript to run this app.</noscript>
                     <noscript>
@@ -30,16 +49,6 @@ class MyDocument extends Document {
                                         trackLinks:true,
                                         accurateTrackBounce:true
                                       });`
-                        }}
-                    />
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WLNJJPEY9H"/>
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `window.dataLayer = window.dataLayer || [];
-                                          function gtag(){dataLayer.push(arguments);}
-                                          gtag('js', new Date());
-                                    
-                                          gtag('config', 'G-WLNJJPEY9H');`
                         }}
                     />
                 </body>
--- src/components/Content/InstagramPhoto.js
+++ src/components/Content/InstagramPhoto.js
@@ -1,7 +1,27 @@
+import * as ga from '../../utils/GoogleAnalytics'
+
 function InstagramPhoto({id, uri}) {
+    const trackImage = (uri) => {
+        ga.event({
+            action: "imageOpened",
+            params : {
+                image: uri
+            }
+        })
+    }
+
     return (
         <div className={"photo"} key={id}>
-            <img loading={"lazy"} src={uri} alt="" data-oi-preview={uri} data-oi-fullsize={uri}/>
+            <img
+                loading={"lazy"}
+                src={uri}
+                alt=""
+                data-oi-preview={uri}
+                data-oi-fullsize={uri}
+                onClick={() => {
+                    trackImage(uri)
+                }}
+            />
         </div>
     )
 }
