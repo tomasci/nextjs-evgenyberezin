5dd1537a821591a3ab805f12a6bf55fa6d78b30b
tomasci <zivgta@gmail.com>
2021-09-18 02:57:33 +0300

    Yandex.Metrika fix, title tag fix

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
         return (
             <Html lang={"en"}>
                 <Head>
-                    <title/>
+                    {/*<title/>*/}
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script
                         async
@@ -19,38 +19,39 @@ class MyDocument extends Document {
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                                  page_path: window.location.pathname,
-                                });
-                              `,
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+								gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+								  page_path: window.location.pathname,
+								});
+							  `,
                         }}
                     />
-                </Head>
-                <body>
-                    <noscript>You need to enable JavaScript to run this app.</noscript>
-                    <noscript>
-                        <div>
-                            <img src="https://mc.yandex.ru/watch/51684428" style={{position: 'absolute', left: '-9999px'}} alt="" />
-                        </div>
-                    </noscript>
-                    <Main/>
-                    <NextScript/>
+
+                    {/* Yandex.Metrika counter */}
                     <script
                         dangerouslySetInnerHTML={{
-                            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                                        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                                      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                                
-                                      ym(51684428, "init", {
-                                        clickmap:true,
-                                        trackLinks:true,
-                                        accurateTrackBounce:true
-                                      });`
+                            __html: `
+								   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+								   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+								   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+								
+								   ym(${process.env.NEXT_PUBLIC_YANDEX_METRIKA}, "init", {
+										clickmap:true,
+										trackLinks:true,
+										accurateTrackBounce:true,
+										webvisor:true
+								   });
+							  `,
                         }}
                     />
+                    <noscript><div><img src={"https://mc.yandex.ru/watch/" + process.env.NEXT_PUBLIC_YANDEX_METRIKA} style={{position: 'absolute', left: '-9999px'}} alt="" /></div></noscript>
+                    {/* /Yandex.Metrika counter */}
+                </Head>
+                <body>
+                    <Main/>
+                    <NextScript/>
                 </body>
             </Html>
         )
--- src/utils/GoogleAnalytics.js
+++ src/utils/GoogleAnalytics.js
@@ -3,7 +3,14 @@ export const pageView = (url) => {
     window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
         page_path: url,
     })
+
+    ym(process.env.NEXT_PUBLIC_YANDEX_METRIKA, 'hit', url)
 }
+// export const pageView = (url) => {
+//     window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+//         page_path: url,
+//     })
+// }
 
 // log specific events happening.
 export const event = ({ action, params }) => {
